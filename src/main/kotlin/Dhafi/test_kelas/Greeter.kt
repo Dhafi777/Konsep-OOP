1079fd735b721b47180692abd8b8e23a731f8be7
Dhafi777 <ininama.404@gmail.com>
2024-09-17 11:09:15 +0700

    add Kelas

--- src/main/kotlin/Dhafi/test_kelas/Greeter.kt
+++ src/main/kotlin/Dhafi/test_kelas/Greeter.kt
@@ -1,7 +1,14 @@
 package Dhafi.test_kelas
 
 class Greeter {
+    var text: String = ""
     fun greet() {
         println("Hello object world!")
+    }
+    fun greet(name: String) {
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String {
+        return "$text $name"
     }
 }
